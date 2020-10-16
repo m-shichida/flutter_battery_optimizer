015cdce9dd23466d76c1f7bd53b4c90077076280
ShichidaMasatsugu <shicchi.7913@icloud.com>
2020-10-16 18:21:32 +0900

    最後まで

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,8 @@ class BatteryOptimizerPage extends StatelessWidget {
         child: Column(
           children: <Widget>[
             OptimizerButtons(),
+            BatteryLevelIndicatorPainter(),
+            AppsDrainage(),
             OptimizeNow(),
           ],
         ),
@@ -66,44 +68,216 @@ class _OptimizerButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
-        color: Colors.white,
-        elevation: kElevation,
-        height: 24,
-        padding: EdgeInsets.symmetric(horizontal: 16),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16),
-        ),
-        onPressed: () {},
-        child: Text(
-          text,
-          style: TextStyle(color: kColorTitle, fontSize: 12),
-        ));
+      color: Colors.white,
+      elevation: kElevation,
+      height: 24,
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      onPressed: () {},
+      child: Text(
+        text,
+        style: TextStyle(color: kColorTitle, fontSize: 12),
+      ),
+    );
   }
 }
 
-// ボタン一覧
 class OptimizerButtons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.only(top: 8),
-        child: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: <Widget>[
-                SizedBox(width: 16),
-                _OptimizerButton(text: 'Battery Optimizer'),
-                SizedBox(width: 16),
-                _OptimizerButton(text: 'Connection Optimizer'),
-                SizedBox(
-                  width: 16,
-                ),
-                _OptimizerButton(text: 'Memory Optimizer'),
-                SizedBox(width: 16),
-                _OptimizerButton(text: 'Storage Optimizer'),
-                SizedBox(width: 16),
-              ],
+      padding: const EdgeInsets.only(top: 8),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: [
+            SizedBox(width: 16),
+            _OptimizerButton(text: 'Battery Optimizer'),
+            SizedBox(width: 16),
+            _OptimizerButton(text: 'Connection Optimizer'),
+            SizedBox(width: 16),
+            _OptimizerButton(text: 'Memory Optimizer'),
+            SizedBox(width: 16),
+            _OptimizerButton(text: 'Storage Optimizer'),
+            SizedBox(width: 16),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// インジケータ
+class _BatteryLevelIndicatorPainter extends CustomPainter {
+  final double percentage; // バッテリーレベルの割合
+  final double textCircleRadius; // 内側に表示される白丸の割合
+
+  _BatteryLevelIndicatorPainter(
+      {@required this.percentage, @required this.textCircleRadius});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (int i = 1; i < (360 * percentage); i += 5) {
+      final per = i / 360.0;
+      // 割合(0~1.0)からグラデーション色に変換
+      final color = ColorTween(
+        begin: kColorIndicatorBegin,
+        end: kColorIndicatorEnd,
+      ).lerp(per);
+      final paint = Paint()
+        ..color = color
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = 4;
+
+      final spaceLen = 16; // 円とゲージ間の長さ
+      final lineLen = 24; // ゲージの長さ
+      final angle = (2 * pi * per) - (pi / 2); // 0時方向から開始するため-90度ずらす
+
+      // 円の中心座標
+      final offset0 = Offset(size.width * 0.5, size.height * 0.5);
+      // 線の内側部分の座標
+      final offset1 = offset0.translate(
+        (textCircleRadius + spaceLen) * cos(angle),
+        (textCircleRadius + spaceLen) * sin(angle),
+      );
+      final offset2 = offset1.translate(
+        lineLen * cos(angle),
+        lineLen * sin(angle),
+      );
+
+      canvas.drawLine(offset1, offset2, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class BatteryLevelIndicatorPainter extends StatelessWidget {
+  final percentage = 0.7;
+  final size = 164.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+        painter: _BatteryLevelIndicatorPainter(
+            percentage: percentage, textCircleRadius: size * 0.5),
+        child: Container(
+            padding: const EdgeInsets.all(96),
+            child: Material(
+              color: Colors.white,
+              elevation: kElevation,
+              borderRadius: BorderRadius.circular(size * 0.5),
+              child: Center(
+                  child: Text('${percentage * 100}%',
+                      style: TextStyle(color: kColorPink, fontSize: 40))),
             )));
+  }
+}
+
+// アプリ一覧
+class _HorizontalBorder extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 1,
+      margin: EdgeInsets.symmetric(horizontal: 16),
+      color: Colors.grey[200],
+    );
+  }
+}
+
+class _AppColumn extends StatelessWidget {
+  final String name;
+  final Icon icon;
+  final String percentage;
+  const _AppColumn({
+    Key key,
+    @required this.name,
+    @required this.icon,
+    @required this.percentage,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: icon,
+      title: Text(
+        name,
+        style: TextStyle(color: kColorText),
+      ),
+      trailing: Text(
+        percentage,
+        style: TextStyle(color: kColorText),
+      ),
+    );
+  }
+}
+
+class AppsDrainage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          leading: ClipOval(
+            child: Container(
+              color: kColorPurple,
+              child: Icon(Icons.apps, color: Colors.white),
+            ),
+          ),
+          title: Text(
+            'Apps Drainage',
+            style: TextStyle(color: kColorTitle),
+          ),
+          subtitle: Text(
+            'Show the most draining energy application',
+            style: TextStyle(color: kColorText),
+          ),
+        ),
+        Material(
+          color: Colors.white,
+          elevation: kElevation,
+          borderRadius: BorderRadius.circular(16),
+          child: Column(
+            children: [
+              _AppColumn(
+                icon: Icon(Icons.sms, color: Colors.indigo),
+                name: 'SMSApp',
+                percentage: '75%',
+              ),
+              _HorizontalBorder(),
+              _AppColumn(
+                icon: Icon(Icons.live_tv, color: Colors.red),
+                name: 'MovieApp',
+                percentage: '60%',
+              ),
+              _HorizontalBorder(),
+              _AppColumn(
+                icon: Icon(Icons.place, color: Colors.green),
+                name: 'MapApp',
+                percentage: '35%',
+              ),
+              _HorizontalBorder(),
+              _AppColumn(
+                icon: Icon(Icons.local_grocery_store, color: Colors.orange),
+                name: 'ShoppingApp',
+                percentage: '35%',
+              ),
+              _HorizontalBorder(),
+              _AppColumn(
+                icon: Icon(Icons.train, color: Colors.black),
+                name: 'TrainApp',
+                percentage: '15%',
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
 
